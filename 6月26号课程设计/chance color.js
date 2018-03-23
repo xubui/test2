f8d1a0964fe64eada9eca1b95024a6786245a2c5
xubu <327254911@qq.com>
2018-03-23 23:50:47 +0800

    zong

---
+++ 6月26号课程设计/chance color.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jsj on 2017/6/26.
+ */
+function chancecolor() {
+    var r = parseInt(Math.random() * 256);
+    var g = parseInt(Math.random() * 256);
+    var b = parseInt(Math.random() * 256);
+
+    return "rgb("+ r +","+ g +","+ b +")";
+
+}
